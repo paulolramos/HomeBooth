9240e6b89b8eefdef7a43d3384e9ad47ba53f68e
Paulo Ramos <PauloRamos64@gmail.com>
2020-12-20 19:02:40 -0800

    add init studio listing service

---
+++ HomeBooth.Services/StudioListing/StudioListingService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using HomeBooth.Data;
+using HomeBooth.Data.Models;
+
+namespace HomeBooth.Services.StudioListing
+{
+    public class StudioListingService : IStudioListingService
+    {
+        private readonly HomeBoothDbContext _db;
+        public StudioListingService(HomeBoothDbContext homeBoothDbContext)
+        {
+            _db = homeBoothDbContext;
+        }
+
+        public ServiceResponse<Studio> CreateListing(Studio listing)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceResponse<Studio> DeleteListing(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceResponse<List<Studio>> GetAllAvailableListings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceResponse<List<Studio>> GetAllListings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceResponse<Studio> GetListingById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ServiceResponse<Studio> UpdateListing(int id, Studio listing)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
