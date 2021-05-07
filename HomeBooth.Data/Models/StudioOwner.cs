28102429efe959365912f2a89626273ea4d7ebb6
Paulo Ramos <PauloRamos64@gmail.com>
2021-05-07 15:28:03 -0700

    require email for host

--- HomeBooth.Data/Models/StudioOwner.cs
+++ HomeBooth.Data/Models/StudioOwner.cs
@@ -15,6 +15,7 @@ namespace HomeBooth.Data.Models
         [MaxLength(32)]
         public string LastName { get; set; }
 
+        [Required]
         [EmailAddress]
         public string Email { get; set; }
 
