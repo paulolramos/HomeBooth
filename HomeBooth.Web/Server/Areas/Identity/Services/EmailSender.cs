69319f3c780b7b22243633bde20debb222147d94
Paulo Ramos <PauloRamos64@gmail.com>
2021-05-10 15:50:46 -0700

    Email Sender Service

---
+++ HomeBooth.Web/Server/Areas/Identity/Services/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+
+namespace HomeBooth.Web.Server.Areas.Identity.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private EmailSenderOptions Options { get; }
+
+        public EmailSender(IOptions<EmailSenderOptions> options)
+        {
+            Options = options.Value;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient(Options.Host)
+            {
+                Port = Options.Port,
+                Credentials = new NetworkCredential(Options.UserName, Options.Password),
+                EnableSsl = Options.EnableSSL,
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(Options.UserName),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true,
+            };
+
+            mailMessage.To.Add(email);
+
+            return client.SendMailAsync(mailMessage);
+        }
+    }
+}
---
+++ HomeBooth.Web/Server/Areas/Identity/Services/EmailSenderOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace HomeBooth.Web.Server.Areas.Identity.Services
+{
+    public class EmailSenderOptions
+    {
+        public const string EmailSender = "EmailSender";
+
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool EnableSSL { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
