f1e03bccb7422141c40e1352da8b6354f5137be0
Paulo Ramos <PauloRamos64@gmail.com>
2021-05-17 14:14:58 -0700

    add relations to Reservation Model

---
+++ HomeBooth.Data/Migrations/20210517210804_AddRelationsToReservation.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeBooth.Data.Migrations
+{
+    public partial class AddRelationsToReservation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientId",
+                table: "Reservations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_ClientId",
+                table: "Reservations",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_StudioId",
+                table: "Reservations",
+                column: "StudioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_StudioServiceId",
+                table: "Reservations",
+                column: "StudioServiceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_AspNetUsers_ClientId",
+                table: "Reservations",
+                column: "ClientId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Studios_StudioId",
+                table: "Reservations",
+                column: "StudioId",
+                principalTable: "Studios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_StudioServices_StudioServiceId",
+                table: "Reservations",
+                column: "StudioServiceId",
+                principalTable: "StudioServices",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_AspNetUsers_ClientId",
+                table: "Reservations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Studios_StudioId",
+                table: "Reservations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_StudioServices_StudioServiceId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_ClientId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_StudioId",
+                table: "Reservations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_StudioServiceId",
+                table: "Reservations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClientId",
+                table: "Reservations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
--- HomeBooth.Data/Models/Reservation.cs
+++ HomeBooth.Data/Models/Reservation.cs
@@ -8,20 +8,28 @@ namespace HomeBooth.Data.Models
         public int Id { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
-        public int ClientId { get; set; }
-
-        [MaxLength(32)]
-        public string ClientContact { get; set; }
-        public int StudioId { get; set; }
-
-        [MaxLength(32)]
-        public string StudioContact { get; set; }
-        public int StudioServiceId { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public float Price { get; set; }
         public string Comment { get; set; }
         public bool IsConfirmed { get; set; }
         public bool IsCancelled { get; set; }
+
+        [MaxLength(32)]
+        public string ClientContact { get; set; }
+
+        [MaxLength(32)]
+        public string StudioContact { get; set; }
+
+
+        public string ClientId { get; set; }
+        public Client Client { get; set; }
+
+        public int StudioId { get; set; }
+        public Studio Studio { get; set; }
+
+        public int StudioServiceId { get; set; }
+        public StudioService StudioService { get; set; }
+
     }
 }
