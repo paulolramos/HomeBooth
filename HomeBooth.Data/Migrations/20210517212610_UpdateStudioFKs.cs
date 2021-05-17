3330a3fef890c7051b6e44b097ff929ae8d10375
Paulo Ramos <PauloRamos64@gmail.com>
2021-05-17 14:28:13 -0700

    Update Studio FKs

---
+++ HomeBooth.Data/Migrations/20210517212610_UpdateStudioFKs.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeBooth.Data.Migrations
+{
+    public partial class UpdateStudioFKs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Studios_StudioAddresses_AddressId",
+                table: "Studios");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AddressId",
+                table: "Studios",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Studios_StudioAddresses_AddressId",
+                table: "Studios",
+                column: "AddressId",
+                principalTable: "StudioAddresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Studios_StudioAddresses_AddressId",
+                table: "Studios");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AddressId",
+                table: "Studios",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Studios_StudioAddresses_AddressId",
+                table: "Studios",
+                column: "AddressId",
+                principalTable: "StudioAddresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- HomeBooth.Data/Models/Studio.cs
+++ HomeBooth.Data/Models/Studio.cs
@@ -7,18 +7,23 @@ namespace HomeBooth.Data.Models
     public class Studio
     {
         public int Id { get; set; }
-
         [MaxLength(32)]
         public string Name { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
-        public Host Host { get; set; }
-        public StudioAddress Address { get; set; }
         public float Rate { get; set; }
         public string Description { get; set; }
+        public bool IsBooked { get; set; }
+
+
+        public Host Host { get; set; }
+        public string HostId { get; set; }
+
+        public StudioAddress Address { get; set; }
+        public int AddressId { get; set; }
+
         public List<StudioSchedule> Schedules { get; set; }
         public List<StudioItem> Equipment { get; set; }
         public List<StudioService> Services { get; set; }
-        public bool IsBooked { get; set; }
     }
 }
